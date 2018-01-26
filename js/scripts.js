4674cec9c57b9fe71a890dd9468b18d09e2c5e21
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-01-26 10:43:20 -0800

    deletes a comma

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+  //var arr = []; //Basic array
+
+  // var arr = ['thingOne', 22, true, {}, []];
+  // console.log(arr[0], arr[3]); // array index 1
+
+  var myArr = ["It", 7, true, 1990];
+  // console.log(myArr, myArr.length);
+
+  // console.log(typeof(myArr));
+  // console.log(Array.isArray(myArr));
+
+  // var bestTeacher = prompt('Who is the best teacher?', 'Damon');
+  // myArr.push(bestTeacher); //you can use .unshift instead of .push to put it in front of the array
+  // console.log(myArr);
+  // console.log(myArr, 'Before!!!');
+  var removeItem = myArr.slice(0,2);
+  // myArr.pop();
+  console.log(removeItem, myArr); // You can remove an item, but you can still keep it
