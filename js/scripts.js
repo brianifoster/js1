db4e1256528eefded9ca6f984deebc609ce4a329
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-06 20:57:08 -0800

    Took notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,26 @@
 /*TRY IT OUT - Create a function that queries the DOM for the body tag using all of these methods. Log the results to your console.
 The function should take 2 arguments: the DOM selector method the user wants to employ and the name of the DOM selector to be searched for.*/
 
-var userQueryType = prompt("Which type do you want to use?", "Please use either 'tag', 'class', 'id', 'selector', 'selectorAll'");
+// var userQueryType = prompt("Which type do you want to use?", "Please use either 'tag', 'class', 'id', 'selector', 'selectorAll'");
+//
+// function queryDom(queryType, tagToQuery) {
+//   tagToQuery = 'body';
+//   switch(queryType) {
+//     case 'tag':
+//       console.log(document.getElementsByTagName('body')[0]);
+//       break;
+//     case 'class':
+//       console.log(document.getElementsByClassName('bodyTag')[0]);
+//       break;
+//     case 'id':
+//       console.log(document.getElementById('bodyTag');
+//       break;
+//     case 'selector':
+//       console.log(document.querySelector('body'));
+//       break;
+//     case 'selectorAll':
+//       console.log(document.querySelectorAll('body')[0]);
+//   }
+// }
 
-function queryDom(queryType, tagToQuery) {
-  tagToQuery = 'body';
-  switch(queryType) {
-    case 'tag':
-      console.log(document.getElementsByTagName('body')[0]);
-      break;
-    case 'class':
-      console.log(document.getElementsByClassName('bodyTag')[0]);
-      break;
-    case 'id':
-      console.log(document.getElementById('bodyTag');
-      break;
-    case 'selector':
-      console.log(document.querySelector('body'));
-      break;
-    case 'selectorAll':
-      console.log(document.querySelectorAll('body')[0]);
-  }
-}
+//In Javascript, all functions are first class functions because the functions can be passed from one function to another
