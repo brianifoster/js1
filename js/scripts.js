f7fea4730f925d21aef28129c9f28137b66e0a5c
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-06 20:13:24 -0800

    Worked on Try It Out for day6

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 //The DOM is an API.  The DOM is where Javascript, HTML, and CSS meet.
+/*TRY IT OUT - Create a function that queries the DOM for the body tag using all of these methods. Log the results to your console.
+The function should take 2 arguments: the DOM selector method the user wants to employ and the name of the DOM selector to be searched for.*/
+
+var userQueryType = prompt("Which type do you want to use?", "Please use either 'tag', 'class', 'id', 'selector', 'selectorAll'");
+
+function queryDom(queryType, tagToQuery) {
+  tagToQuery = 'body';
+  switch(queryType) {
+    case 'tag':
+      console.log(document.getElementsByTagName('body')[0]);
+      break;
+    case 'class':
+      console.log(document.getElementsByClassName('bodyTag')[0]);
+      break;
+    case 'id':
+      console.log(document.getElementById('bodyTag');
+      break;
+    case 'selector':
+      console.log(document.querySelector('body'));
+      break;
+    case 'selectorAll':
+      console.log(document.querySelectorAll('body')[0]);
+  }
+}
