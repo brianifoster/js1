46c78536845c65a66be4421d576c6d37b85aa0e5
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-13 13:57:30 -0800

    My attempt at the take home work for day7

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,74 @@
-var h1 = document.createElement("h1");
-var p = document.createElement('p');
-var body = document.querySelector("body");
+// var h1 = document.createElement("h1");
+// var p = document.createElement('p');
+// var body = document.querySelector("body");
+//
+// h1.innerHTML = "<h1>Hello world</h1>";
+// p.textContent = "whoa!!!";
+//
+// body.appendChild(h1);
+// body.appendChild(p);
+// //create a new element
+// var newDiv = document.createElement("div");
+// //add height and color to see it
+// newDiv.style.backgroundColor = "green";
+// newDiv.style.height = "120px";
+//
+// body.insertBefore(newDiv, p);
+//
+// // body.classList.add("red-background");
+// // body.classList.remove("red-background");
+// body.classList.toggle('red-background');//if it exists, take it off; if it doesn't exist, turn it on.
+//
+// body.classList.contains("white-text");//returns a boolean
+//
+// body.classList.replace("white-text", "purple-text");//add/remove together
+//
+// var newImg = document.createElement('img');
+// newImg.setAttribute("src", "https://s3.amazonaws.com/ffe-ugc/intlportal2/dev-temp/en-US/__59a48ed7599d0.jpg");
+//
+// body.appendChild(newImg);
 
-h1.innerHTML = "<h1>Hello world</h1>";
-p.textContent = "whoa!!!";
 
-body.appendChild(h1);
-body.appendChild(p);
-//create a new element
-var newDiv = document.createElement("div");
-//add height and color to see it
-newDiv.style.backgroundColor = "green";
-newDiv.style.height = "120px";
+//TAKE HOME 7
+//Step 1 - When the user clicks a button, prompt them for the a title.
+var myButton = document.createElement('button');
+var body = document.querySelector('body');
+myButton.innerHTML = "Click Me!";
+var form = document.querySelector('form');
 
-body.insertBefore(newDiv, p);
+body.insertBefore(myButton, form);
 
-// body.classList.add("red-background");
-// body.classList.remove("red-background");
-body.classList.toggle('red-background');//if it exists, take it off; if it doesn't exist, turn it on.
+myButton.addEventListener('click', titlePrompt);
 
-body.classList.contains("white-text");//returns a boolean
+function titlePrompt() {
+  var userTitle = prompt('Give me a title');
+  //Step 2 - Put that title into an h1 that is centered on the page.
+  var h1 = document.createElement('h1');
 
-body.classList.replace("white-text", "purple-text");//add/remove together
+  h1.innerHTML = userTitle;
+  h1.style.textAlign = 'center';
+  body.appendChild(h1);
+}
 
-var newImg = document.createElement('img');
-newImg.setAttribute("src", "https://s3.amazonaws.com/ffe-ugc/intlportal2/dev-temp/en-US/__59a48ed7599d0.jpg");
+var myForm = document.createElement('form');
+var textArea = document.createElement('textarea');
+var myInput = document.createElement('input');
+textArea.innerHTML = "Add a few sentences about what you did today";
+myInput.setAttribute("type", "Submit");
+myInput.innerHTML = "Enter";
+textArea.style.width = "300px";
+textArea.style.height = "200px";
+form.appendChild(textArea);
+form.appendChild(myInput);
 
-body.appendChild(newImg);
+myInput.addEventListener('click', sentencePrompt);
+
+function sentencePrompt() {
+  var userSentences = console.log(textArea);
+}
+
+
+
+
+
+//Step 3 - Ask the user to write a few sentences about what they did today.
