8b936b782ac50b2be3d70f0dc8449beb04dbf6d7
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-13 18:23:02 -0800

    Did FizzBuzz test

--- js/scripts.js
+++ js/scripts.js
@@ -31,47 +31,55 @@
 
 //TAKE HOME 7
 //Step 1 - When the user clicks a button, prompt them for the a title.
-var myButton = document.createElement('button');
-var body = document.querySelector('body');
-myButton.innerHTML = "Click Me!";
-var form = document.querySelector('form');
-
-body.insertBefore(myButton, form);
-
-myButton.addEventListener('click', titlePrompt);
-
-function titlePrompt() {
-  var userTitle = prompt('Give me a title');
-  //Step 2 - Put that title into an h1 that is centered on the page.
-  var h1 = document.createElement('h1');
-
-  h1.innerHTML = userTitle;
-  h1.style.textAlign = 'center';
-  body.appendChild(h1);
-}
-
-var myForm = document.createElement('form');
-var textArea = document.createElement('textarea');
-var myInput = document.createElement('input');
-textArea.innerHTML = "Add a few sentences about what you did today";
-myInput.setAttribute("type", "Submit");
-textArea.style.width = "300px";
-textArea.style.height = "200px";
-form.appendChild(textArea);
-form.appendChild(myInput);
-
-myInput.addEventListener('click', function();
-
-function sentencePrompt() {
-  var userSentences = console.log(textArea);
-  var para = document.createElement('p');
-  para.innerHTML = textArea;
-});
-
-
-
-
-
+// var myButton = document.createElement('button');
+// var body = document.querySelector('body');
+// myButton.innerHTML = "Click Me!";
+// var form = document.querySelector('form');
+//
+// body.insertBefore(myButton, form);
+//
+// myButton.addEventListener('click', titlePrompt);
+//
+// function titlePrompt() {
+//   var userTitle = prompt('Give me a title');
+//   //Step 2 - Put that title into an h1 that is centered on the page.
+//   var h1 = document.createElement('h1');
+//
+//   h1.innerHTML = userTitle;
+//   h1.style.textAlign = 'center';
+//   body.appendChild(h1);
+// }
+//
+// var myForm = document.createElement('form');
+// var textArea = document.createElement('textarea');
+// var myInput = document.createElement('input');
+// textArea.innerHTML = "Add a few sentences about what you did today";
+// myInput.setAttribute("type", "Submit");
+// textArea.style.width = "300px";
+// textArea.style.height = "200px";
+// form.appendChild(textArea);
+// form.appendChild(myInput);
+//
+// myInput.addEventListener('click', function();
+//
+// function sentencePrompt() {
+//   var userSentences = console.log(textArea);
+//   var para = document.createElement('p');
+//   para.innerHTML = textArea;
+// });
 
 
 //Step 3 - Ask the user to write a few sentences about what they did today.
+
+//FizzsBuzz Test
+
+// for (let i = 0; i <= 100; i++) {
+//   if(i % 3 ===0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if(i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if(i % 5 === 0) {
+//     console.log('Buzz');
+//   }
+//   console.log(i);
+// }
