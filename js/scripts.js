a23a324b9b0082dd2cbac49ac0910b417bea8e94
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-15 20:43:52 -0800

    Finishes day 9

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,14 @@
 // console.log(newPerson);
 // var aBC = prompt("What's your username?");
 // var xYZ = prompt("What's your password?");
+var newUserForm = document.forms.newUser;
+newUserForm.addEventListener("submit", function(e) {
+  login(e);
+});
+
+// submit.addEventListener('click', function() {
+//
+// })
 
 function Users(username, password) {
   this.username = username;
@@ -33,12 +41,34 @@ function Users(username, password) {
 
 var users = [];
 
-function login() {
-  var username = prompt("Give me your username");
-  var password = prompt("Password, please...");
+function login(e) {
+  e.preventDefault();//stops it from actually submitting the information
+  var username = document.querySelector('.username').value;
+  var password = newUserForm.password.value;
   var userInfo = new Users(username, password);
   users.push(users);
   console.log(userInfo);
 }
 
-login();
+var calcMileage = function(miles, gallons) {
+  return miles / gallons;
+}
+
+function Vehicle(wheels, type, engine, color, miles, gallons) {
+  this.wheels = wheels;
+  this.type = type;
+  this.engine = engine;
+  this.color = color;
+  this.calculateGasMileage = calcMileage(miles, gallons);
+}
+
+function Engine(numOfCycls, injectionSystem, vrooms, horsees) {
+  this.numOfCycls = numOfCycls;
+  this.injectionSystem = injectionSystem;
+  this.vrooms = vrooms;
+  this.hp = horsees;
+}
+var myEngine = new Engine(2, 'crap', 0.00, "???");
+
+var saturn = new Vehicle(4, "car", myEngine, "faded grey", 5, 4);
+console.log(saturn);
