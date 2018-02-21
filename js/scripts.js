83f708f3ff29ab04144d9a1bae18f23948ee7038
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-20 18:31:19 -0800

    notes on sessionStorage & localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-function log(string) {
-  alert(string);
-};
+// function log(string) {
+//   alert(string);
+// };
+//
+// var myTimer = setTimeout(function() {
+//   log("Buy stuff!!!");
+// }, 15000)
+//
+// if(user.login == true) {
+//   clearTimeout(myTimer)
+// }
+// log("I'm first!");
 
-var myTimer = setTimeout(function() {
-  log("Buy stuff!!!");
-}, 15000)
+// Session Storage
+//.setItem
+sessionStorage.setItem("user", {"username": "brianifoster", "role": "superadmin"});
+//.getItem
+var userRole = sessionStorage.getItem('user');
+console.log(userRole);
+//.removeItem
+sessionStorage.removeItem("username"); //used if you want to replace with something
+//.clear
+sessionStorage.clear(); //it clears everything that was stored
 
-if(user.login == true) {
-  clearTimeout(myTimer)
-}
-log("I'm first!");
+//localStorage also works just like sessionStorage, except for .clear
